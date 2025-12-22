14d220d1119dc2457be08742c7fc2b197d57838c
zach lilley <zlilley96@gmail.com>
2025-12-22 11:45:00 -0500

    Initial commit

---
+++ src/main/java/com/lilley/modernnoise/ModernnoiseApplication.java
@@ -0,0 +1,13 @@
+package com.lilley.modernnoise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModernnoiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModernnoiseApplication.class, args);
+	}
+
+}
