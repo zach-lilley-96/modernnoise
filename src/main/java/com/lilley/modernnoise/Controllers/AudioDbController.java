fa50885bc487c00e4b2c3da9594614aa61f2423f
zach lilley <zlilley96@gmail.com>
2025-12-29 22:03:48 -0500

    Constructor refactor

--- src/main/java/com/lilley/modernnoise/Controllers/AudioDbController.java
+++ src/main/java/com/lilley/modernnoise/Controllers/AudioDbController.java
@@ -4,6 +4,7 @@ import com.lilley.modernnoise.Data.Dtos.AlbumDto;
 import com.lilley.modernnoise.Data.Dtos.ArtistDto;
 import com.lilley.modernnoise.RestClients.impl.AudioDbRestClient;
 import com.lilley.modernnoise.Services.AudioDbService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
