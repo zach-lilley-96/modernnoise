55274229c71dd4dfe148c42b224a790e3c5baa05
zach lilley <zlilley96@gmail.com>
2026-01-03 22:59:48 -0500

    Fix service error

--- src/main/java/com/lilley/modernnoise/Controllers/AudioDbController.java
+++ src/main/java/com/lilley/modernnoise/Controllers/AudioDbController.java
@@ -3,6 +3,7 @@ package com.lilley.modernnoise.Controllers;
 import com.lilley.modernnoise.Data.Dtos.AlbumDto;
 import com.lilley.modernnoise.Data.Dtos.ArtistDto;
 import com.lilley.modernnoise.RestClients.impl.AudioDbRestClient;
+import com.lilley.modernnoise.Services.ArtistService;
 import com.lilley.modernnoise.Services.AudioDbService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,21 +15,22 @@ import java.util.List;
 
 @RestController
 @RequestMapping("audiodb")
+@RequiredArgsConstructor
 public class AudioDbController {
     private final AudioDbService audioDbService;
-
-    public AudioDbController(AudioDbService audioDbService) {
-        this.audioDbService = audioDbService;
-    }
-
+    private final ArtistService artistService;
 
     @GetMapping("/artist/{artistName}")
     public List<ArtistDto> searchArtistByName(@PathVariable String artistName) {
         return audioDbService.GetArtistData(artistName);
     }
 
-    @GetMapping("/albums/{artistName}")
-    public List<AlbumDto> getAlbumsByArtistName(@PathVariable String artistName) {
-        return audioDbService.GetAlbumsByArtist(artistName);
+    @GetMapping("/albums/{artistAudioDbId}")
+    public List<AlbumDto> getAlbumsByArtistName(@PathVariable String artistAudioDbId) {
+        var existingAlbums = artistService.getAlbumsByMusicBrainzId(artistAudioDbId);
+        if (!existingAlbums.isEmpty()) {
+            return existingAlbums;
+        }
+        return audioDbService.GetAlbumsByArtist(artistAudioDbId);
     }
 }
