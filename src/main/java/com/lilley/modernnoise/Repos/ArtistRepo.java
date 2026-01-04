15c4f5bd7c31dd19afd3458c25489e5ffc2ae3d0
zach lilley <zlilley96@gmail.com>
2026-01-03 23:22:19 -0500

    Ignore casing

--- src/main/java/com/lilley/modernnoise/Repos/ArtistRepo.java
+++ src/main/java/com/lilley/modernnoise/Repos/ArtistRepo.java
@@ -4,12 +4,14 @@ import com.lilley.modernnoise.Data.Entities.Artist;
 import jakarta.validation.constraints.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface ArtistRepo extends JpaRepository<Artist, UUID> {
-    Artist findByName(@NotNull String name);
+//    Artist findByName(@NotNull String name);
     Artist findByAudioDbId(@NotNull String audioDbId);
     boolean existsByAudioDbId(@NotNull String audioDbId);
     boolean existsByName(@NotNull String name);
+    Optional<Artist> findByNameIgnoreCase(@NotNull String name);
 
 }
