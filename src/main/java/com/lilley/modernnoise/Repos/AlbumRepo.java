6eb79ec5cd4b2fc16ddc05355968e078f8d9d10f
zach lilley <zlilley96@gmail.com>
2026-01-03 19:31:47 -0500

    Sort by release year

--- src/main/java/com/lilley/modernnoise/Repos/AlbumRepo.java
+++ src/main/java/com/lilley/modernnoise/Repos/AlbumRepo.java
@@ -3,8 +3,10 @@ package com.lilley.modernnoise.Repos;
 import com.lilley.modernnoise.Data.Entities.Album;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface AlbumRepo extends JpaRepository<Album, UUID> {
     boolean existsByAudioDbId(String audioDbId);
+    List<Album> findByArtistIdOrderByReleaseYear(UUID artistId);
 }
