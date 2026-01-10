392adad5a048f3cbe399143b665414045687f86c
zach lilley <zlilley96@gmail.com>
2026-01-10 16:49:38 -0500

    Add cors

--- src/main/java/com/lilley/modernnoise/Controllers/SearchController.java
+++ src/main/java/com/lilley/modernnoise/Controllers/SearchController.java
@@ -5,6 +5,7 @@ import com.lilley.modernnoise.Data.Dtos.ArtistDto;
 import com.lilley.modernnoise.Services.ArtistService;
 import com.lilley.modernnoise.Services.AudioDbService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,17 +19,23 @@ public class SearchController {
     private final AudioDbService audioDbService;
     private final ArtistService artistService;
 
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
     @GetMapping("/artist/{artistName}")
     public List<ArtistDto> searchArtistByName(@PathVariable String artistName) {
         return audioDbService.GetArtistData(artistName);
     }
 
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
     @GetMapping("/albums/{artistAudioDbId}")
-    public List<AlbumDto> getAlbumsByArtistName(@PathVariable String artistAudioDbId) {
+    public ResponseEntity<List<AlbumDto>> getAlbumsByArtistName(@PathVariable String artistAudioDbId) {
         var existingAlbums = artistService.getAlbumsByMusicBrainzId(artistAudioDbId);
         if (!existingAlbums.isEmpty()) {
-            return existingAlbums;
+            return ResponseEntity.ok(existingAlbums);
         }
-        return audioDbService.GetAlbumsByArtist(artistAudioDbId);
+        try{
+        return ResponseEntity.ok(audioDbService.GetAlbumsByArtist(artistAudioDbId));
+        } catch (Exception e){
+            return ResponseEntity.badRequest().build();
+        }
     }
 }
