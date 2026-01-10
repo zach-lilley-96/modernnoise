c755544ee03290806aaf47dacffd9630038dafe8
zach lilley <zlilley96@gmail.com>
2026-01-10 16:49:29 -0500

    Add route for checking auth status

--- src/main/java/com/lilley/modernnoise/Controllers/Auth/AuthController.java
+++ src/main/java/com/lilley/modernnoise/Controllers/Auth/AuthController.java
@@ -3,7 +3,9 @@ package com.lilley.modernnoise.Controllers.Auth;
 import com.lilley.modernnoise.Data.Entities.User;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseCookie;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,13 +13,23 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-@RequestMapping("/auth")
+import java.security.Principal;
+
+@RestController
+@RequestMapping("auth")
 public class AuthController {
 
     @GetMapping("/me")
     public User me(@AuthenticationPrincipal User user){
         return user;
+    }
+
+    @GetMapping("/status")
+    public ResponseEntity<Void> checkStatus(Principal principal) {
+        if (principal == null) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+        return ResponseEntity.ok().build();
     }
     @PostMapping("/logout")
     public void logout(HttpServletResponse response){
