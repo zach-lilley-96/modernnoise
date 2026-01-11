e6b69c4c2d3b471b1059fb0dcfb075396f16c7bd
zach lilley <zlilley96@gmail.com>
2026-01-11 12:48:50 -0500

    Add method for retrieving album by audioDbId

--- src/main/java/com/lilley/modernnoise/Repos/AlbumRepo.java
+++ src/main/java/com/lilley/modernnoise/Repos/AlbumRepo.java
@@ -3,10 +3,13 @@ package com.lilley.modernnoise.Repos;
 import com.lilley.modernnoise.Data.Entities.Album;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import javax.swing.text.html.Option;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface AlbumRepo extends JpaRepository<Album, UUID> {
     boolean existsByAudioDbId(String audioDbId);
     List<Album> findByArtistIdOrderByReleaseYear(UUID artistId);
+    Optional<Album> findByAudioDbId(String audioDbId);
 }
