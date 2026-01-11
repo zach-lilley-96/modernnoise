f6eb8dd55b48b80d9d7f5cfa86d3693a8606d489
zach lilley <zlilley96@gmail.com>
2026-01-10 23:23:02 -0500

    Get albums by musicBrainz ID

--- src/main/java/com/lilley/modernnoise/Services/AlbumService.java
+++ src/main/java/com/lilley/modernnoise/Services/AlbumService.java
@@ -3,9 +3,11 @@ package com.lilley.modernnoise.Services;
 import com.lilley.modernnoise.Data.Dtos.AlbumDto;
 import com.lilley.modernnoise.Mappers.AlbumMapper;
 import com.lilley.modernnoise.Repos.AlbumRepo;
+import com.lilley.modernnoise.Repos.ArtistRepo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.UUID;
 
@@ -13,11 +15,24 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class AlbumService {
     private final AlbumRepo albumRepo;
+    private final ArtistRepo artistRepo;
 
     public List<AlbumDto> getAlbumsByArtistId(UUID artistId) {
         return albumRepo.findByArtistIdOrderByReleaseYear(artistId)
                 .stream()
                 .map(AlbumMapper::toDto)
+                .toList();
+    }
+
+    public List<AlbumDto> getAlbumsByMusicBrainzId(String audioDbId) {
+        var artist = artistRepo.findByAudioDbId(audioDbId);
+        if (artist == null) {
+            return List.of();
+        }
+
+        return artist.getAlbums().stream()
+                .map(AlbumMapper::toDto)
+                .sorted(Comparator.comparing(AlbumDto::intYearReleased))
                 .toList();
     }
 }
