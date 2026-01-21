fa38669ffe30f703ae0b1fc26d8158e778511f6a
zach lilley <zlilley96@gmail.com>
2026-01-20 22:45:35 -0500

    Global exceptions

--- src/main/java/com/lilley/modernnoise/Exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/lilley/modernnoise/Exceptions/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.lilley.modernnoise.Exceptions;
 
 import com.lilley.modernnoise.Data.Dtos.Response.ErrorResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -8,11 +9,13 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 
 @RestControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
 
     // Handle specific exceptions (e.g., Resource Not Found)
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<ErrorResponse> handleIllegalArgumentException(IllegalArgumentException ex) {
+        log.error("IllegalArgumentException: {}", ex.getMessage());
         ErrorResponse error = new ErrorResponse(
                 ex.getMessage(),
                 HttpStatus.BAD_REQUEST.value()
@@ -23,6 +26,7 @@ public class GlobalExceptionHandler {
     // Handle security or unauthorized access if needed
     @ExceptionHandler(RuntimeException.class)
     public ResponseEntity<ErrorResponse> handleRuntimeException(RuntimeException ex) {
+        log.error("RuntimeException: ", ex);
         ErrorResponse error = new ErrorResponse(
                 "An internal error occurred: " + ex.getMessage(),
                 HttpStatus.INTERNAL_SERVER_ERROR.value()
@@ -33,6 +37,7 @@ public class GlobalExceptionHandler {
     // Generic handler for all other exceptions
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> handleGlobalException(Exception ex, WebRequest request) {
+        log.error("Unexpected error: ", ex);
         ErrorResponse error = new ErrorResponse(
                 "Unexpected error: " + ex.getMessage(),
                 HttpStatus.INTERNAL_SERVER_ERROR.value()
