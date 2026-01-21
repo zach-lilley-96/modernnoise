1000a38d2dc437792d281b993f23d9df0af81461
zach lilley <zlilley96@gmail.com>
2026-01-20 22:44:56 -0500

    Redirect URL

--- src/main/java/com/lilley/modernnoise/Services/Auth/OAuthUserService.java
+++ src/main/java/com/lilley/modernnoise/Services/Auth/OAuthUserService.java
@@ -3,19 +3,23 @@ package com.lilley.modernnoise.Services.Auth;
 import com.lilley.modernnoise.Data.Entities.User;
 import com.lilley.modernnoise.Repos.UserRepo;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class OAuthUserService {
     private final UserRepo userRepo;
 
     public User processOAuthUser(OAuth2User oAuthUser){
         String email = oAuthUser.getAttribute("email");
         String displayName = oAuthUser.getAttribute("name");
+        log.info("Processing OAuth user: {}", email);
 
         return userRepo.findByEmail(email).orElseGet(() -> {
+            log.info("User {} not found, creating new user profile.", email);
             User newUser = User.builder()
                     .email(email)
                     .displayName(displayName)
--- src/main/java/com/lilley/modernnoise/Utils/Auth/OAuthSuccessHandler.java
+++ src/main/java/com/lilley/modernnoise/Utils/Auth/OAuthSuccessHandler.java
@@ -7,6 +7,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.jspecify.annotations.NonNull;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseCookie;
 import org.springframework.security.core.Authentication;
