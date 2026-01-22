cf85dc52dfc7862f6e12636f2c28d241d1ce02eb
zach lilley <zlilley96@gmail.com>
2026-01-21 23:11:19 -0500

    Quick fix

--- src/main/java/com/lilley/modernnoise/Config/SecurityConfig.java
+++ src/main/java/com/lilley/modernnoise/Config/SecurityConfig.java
@@ -51,7 +51,7 @@ public class SecurityConfig {
     @Bean
     UrlBasedCorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173", "https://modernnoise-ui.vercel.app/"));
+        config.setAllowedOrigins(List.of("http://localhost:5173", "https://modernnoise-ui.vercel.app"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
