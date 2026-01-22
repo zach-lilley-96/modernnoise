ad9aab06f29b1578bd92b3ec3851aaf94a11d517
zach lilley <zlilley96@gmail.com>
2026-01-21 23:31:21 -0500

    remove unused import

--- src/main/java/com/lilley/modernnoise/Controllers/Auth/AuthController.java
+++ src/main/java/com/lilley/modernnoise/Controllers/Auth/AuthController.java
@@ -8,14 +8,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
 
+@CrossOrigin(origins = "https://modernnoise-ui.vercel.app", allowedHeaders = "*")
 @RestController
 @Slf4j
 @RequestMapping("auth")
