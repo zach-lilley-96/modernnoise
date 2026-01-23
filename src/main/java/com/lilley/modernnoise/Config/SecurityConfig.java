86a7a2ef09bb329b6d11bff32dd41fe143fd1352
zach lilley <zlilley96@gmail.com>
2026-01-22 22:04:00 -0500

    Change header strategy for Render

--- src/main/java/com/lilley/modernnoise/Config/SecurityConfig.java
+++ src/main/java/com/lilley/modernnoise/Config/SecurityConfig.java
@@ -67,7 +67,7 @@ public class SecurityConfig {
     @Bean
     UrlBasedCorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173", "https://modernnoise-ui.vercel.app"));
+        config.setAllowedOrigins(List.of("http://localhost:5173", "https://modernnoise-ui.onrender.com/"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
