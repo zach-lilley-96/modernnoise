ab031ab8196a9933e6cbfe607cb500d95062dc96
nose <zxdxle813@gmail.com>
2026-02-05 00:57:37 -0500

    Play with controller

--- src/main/java/com/lilley/modernnoise/Controllers/FriendController.java
+++ src/main/java/com/lilley/modernnoise/Controllers/FriendController.java
@@ -1,53 +1,57 @@
 package com.lilley.modernnoise.Controllers;
 
-import com.lilley.modernnoise.Data.Entities.User;
-import com.lilley.modernnoise.Repos.UserRepo;
-import com.lilley.modernnoise.Services.FriendService;
-import lombok.RequiredArgsConstructor;
+import java.util.UUID;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.UUID;
+import com.lilley.modernnoise.Data.Entities.User;
+import com.lilley.modernnoise.Services.FriendService;
+
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/friends")
 @RequiredArgsConstructor
 public class FriendController {
+
     private final FriendService friendService;
-    private final UserRepo userRepo;
 
     @GetMapping("/friendCode")
-    public ResponseEntity<UUID> GetFriendCode(@AuthenticationPrincipal User user){
+    public ResponseEntity<UUID> GetFriendCode(@AuthenticationPrincipal User user) {
         return ResponseEntity.ok(friendService.GetFriendCode(user));
     }
 
     @GetMapping("/generate")
-    public ResponseEntity<UUID> GenerateFriendCode(@AuthenticationPrincipal User user){
+    public ResponseEntity<UUID> GenerateFriendCode(@AuthenticationPrincipal User user) {
         return ResponseEntity.ok(friendService.GenerateFriendCode(user));
     }
 
     @GetMapping("/me")
-    public ResponseEntity<?> GetFriends(@AuthenticationPrincipal User user){
+    public ResponseEntity<?> GetFriends(@AuthenticationPrincipal User user) {
         return ResponseEntity.ok(friendService.GetFriends(user));
     }
 
-
     @PostMapping("/search")
-    public ResponseEntity<?> SearchForUserByFriendCode(@RequestBody String friendCode){
+    public ResponseEntity<?> SearchForUserByFriendCode(@RequestBody String friendCode) {
         return ResponseEntity.ok(friendService.GetUsernameByFriendCode(UUID.fromString(friendCode)));
     }
 
-
     @PostMapping
-    public ResponseEntity<?> AddFriend(@AuthenticationPrincipal User user, @RequestBody String friendCode){
+    public ResponseEntity<?> AddFriend(@AuthenticationPrincipal User user, @RequestBody String friendCode) {
         var friendCodeUUID = UUID.fromString(friendCode);
         friendService.AddFriend(user, friendCodeUUID);
         return ResponseEntity.ok().build();
     }
 
     @DeleteMapping
-    public ResponseEntity<?> RemoveFriend(@AuthenticationPrincipal User user, @RequestBody String friendCode){
+    public ResponseEntity<?> RemoveFriend(@AuthenticationPrincipal User user, @RequestBody String friendCode) {
         var friendCodeUUID = UUID.fromString(friendCode);
         friendService.RemoveFriend(user, friendCodeUUID);
         return ResponseEntity.ok().build();
