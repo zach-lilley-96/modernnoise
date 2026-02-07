6ddfba4bb132f0308ce2f6ec1c2904a1894aee25
zach lilley <zlilley96@gmail.com>
2026-02-07 16:53:11 -0500

    Logging and stricter typing

--- src/main/java/com/lilley/modernnoise/Services/FriendService.java
+++ src/main/java/com/lilley/modernnoise/Services/FriendService.java
@@ -1,41 +1,44 @@
 package com.lilley.modernnoise.Services;
 
 import com.lilley.modernnoise.Data.Dtos.FriendDto;
+import com.lilley.modernnoise.Data.Dtos.Response.SuccessfulFriendCodeResponse;
 import com.lilley.modernnoise.Data.Entities.User;
 import com.lilley.modernnoise.Repos.UserRepo;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.java.Log;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.UUID;
 
 @Service
+@Slf4j
 @RequiredArgsConstructor
 public class FriendService {
     private final UserRepo userRepo;
 
     @Transactional
-    public void AddFriend(User user, UUID friendCode){
-        var friendOpt = userRepo.findByFriendCode(friendCode);
+    public void AddFriend(UUID userId, UUID friendCode){
+        var friendOpt = userRepo.findByFriendCode(friendCode).orElseThrow(() -> new IllegalArgumentException("Friend not found"));
 
-        if(friendOpt.isEmpty()){
-            throw new IllegalArgumentException("User or Friend not found");
-        }
+        var user = userRepo.findById(userId).orElseThrow(() -> new IllegalArgumentException("User not found"));
+
         var friendSet = user.getFriendSet();
-        if (friendSet.contains(friendOpt.get())){
+        if (friendSet.contains(friendOpt)){
             return;
         }
-        user.getFriendSet().add(friendOpt.get());
+        user.getFriendSet().add(friendOpt);
         userRepo.save(user);
     }
 
-    public String GetUsernameByFriendCode(UUID friendCode){
+    public SuccessfulFriendCodeResponse GetUsernameByFriendCode(UUID friendCode){
         var friendOpt = userRepo.findByFriendCode(friendCode);
         if(friendOpt.isEmpty()){
             throw new IllegalArgumentException("Friend not found");
         }
-        return friendOpt.get().getDisplayName();
+        return new SuccessfulFriendCodeResponse(friendOpt.get().getDisplayName());
     }
 
     @Transactional
@@ -68,9 +71,11 @@ public class FriendService {
         return newFriendCode;
     }
 
-
-    public List<FriendDto> GetFriends(User user){
+    @Transactional
+    public List<FriendDto> GetFriends(UUID userId){
+        var user = userRepo.findById(userId).orElseThrow(() -> new IllegalArgumentException("User not found"));
         var friendSet = user.getFriendSet();
+        log.info("User {} has {} friends.", user.getDisplayName(), friendSet.size());
         return friendSet.stream()
                 .map(friend -> new FriendDto(friend.getFriendCode(), friend.getDisplayName()))
                 .toList();
