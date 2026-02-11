b16ef0279e45c90483b9b60926fa300e2948e3c9
zach lilley <zlilley96@gmail.com>
2026-02-10 20:06:54 -0500

    Thread pooling

--- src/main/java/com/lilley/modernnoise/Config/AsyncConfig.java
+++ src/main/java/com/lilley/modernnoise/Config/AsyncConfig.java
@@ -1,9 +1,23 @@
 package com.lilley.modernnoise.Config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
 
 @Configuration
 @EnableAsync
 public class AsyncConfig {
+    @Bean(name = "taskExecutor")
+    public Executor customExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(4);
+        executor.setMaxPoolSize(8);
+        executor.setQueueCapacity(50);
+        executor.setThreadNamePrefix("Worker-");
+        executor.initialize();
+        return executor;
+    }
 }
