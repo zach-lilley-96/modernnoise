f14dc9b977124e4d05d378367107315b248bdc7e
zach lilley <zlilley96@gmail.com>
2026-02-15 11:57:39 -0500

    Add registration email after first login

--- src/main/java/com/lilley/modernnoise/Services/Auth/OAuthUserService.java
+++ src/main/java/com/lilley/modernnoise/Services/Auth/OAuthUserService.java
@@ -1,7 +1,10 @@
 package com.lilley.modernnoise.Services.Auth;
 
+import com.lilley.modernnoise.Data.Dtos.Mail.EmailDetails;
+import com.lilley.modernnoise.Data.Dtos.Mail.MailType;
 import com.lilley.modernnoise.Data.Entities.User;
 import com.lilley.modernnoise.Repos.UserRepo;
+import com.lilley.modernnoise.Services.Interfaces.MailService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.oauth2.core.user.OAuth2User;
@@ -12,6 +15,7 @@ import org.springframework.stereotype.Service;
 @Slf4j
 public class OAuthUserService {
     private final UserRepo userRepo;
+    private final MailService mailService;
 
     public User processOAuthUser(OAuth2User oAuthUser){
         String email = oAuthUser.getAttribute("email");
@@ -25,7 +29,15 @@ public class OAuthUserService {
                     .displayName(displayName)
                     .provider("GOOGLE")
                     .build();
-            return userRepo.save(newUser);
+
+            // Make sure the user obj saves properly before sending registration email
+            var user = userRepo.save(newUser);
+            mailService.sendEmail( new EmailDetails(
+                    email,
+                    "Welcome to ModernNoise!",
+                    "Thank you for signing up with ModernNoise. Enjoy your stay! You can turn on other email notifications including new friend and new ranking notifications from the home screen.",
+                    MailType.REGISTRATION));
+            return user;
         });
     }
 }
